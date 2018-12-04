7a0fc9df69d7278ef9a1ce4412c282897f17b32d
Psanyi89 <pleskosandor@gmail.com>
2018-12-04 22:43:25 +0100

    latest

---
+++ Apple_and_Orange/Apple_and_Orange/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+internal class Solution
+{
+
+    // Complete the countApplesAndOranges function below.
+    private static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
+        int alma = 0;
+        int narancs = 0;
+        for (int i = 0; i < apples.Length; i++)
+        {
+            if ((apples[i] + a) >= s && (apples[i] + a) <= t)
+            {
+                alma++;
+            }
+        }
+        for (int i = 0; i < oranges.Length; i++)
+        {
+
+            if ((oranges[i] + b) >= s && (oranges[i] + b) <= t)
+            {
+                narancs++;
+            }
+        }
+
+        Console.WriteLine(alma);
+        Console.WriteLine(narancs);
+
+    }
+
+    private static void Main(string[] args)
+    {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
---
+++ Hacherrank/Hacherrank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+internal class Solution
+{
+
+    // Complete the birthday function below.
+    private static int birthday(List<int> s, int d, int m)
+    {
+        int count = 0;
+        for (int i = 0; i <= s.Count - m; i++)
+        {
+            int sum = s.GetRange(i, m).Aggregate((a, b) => a + b);
+            if (sum == d)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(dm[0]);
+
+        int m = Convert.ToInt32(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
